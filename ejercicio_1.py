ad8b6e6ddcdf51faf7ea7fb75bc4578deb66fd34
Angel Zamorano <angel.zamorano@innova-ti.cl>
2023-05-09 13:57:05 -0400

    termino de ejercicio 1

---
+++ ejercicio_1.py
@@ -0,0 +1,25 @@
+
+
+validador = True
+arreglo = []
+print(' Ingrese numeros para ser ordenados de mayor a menor, el 0 es para salir del programa')
+while validador:
+    numeroIngresado = int(input('Ingrese numero:  '))
+    
+    if numeroIngresado !=0:
+        arreglo.append(numeroIngresado)
+    else:
+        validador = False
+
+print(arreglo)
+
+for x in range(len(arreglo)):
+    for i in range(0,len(arreglo)-x-1):
+        if arreglo[i] < arreglo[i+1]:
+            aux = arreglo[i]
+            arreglo[i] = arreglo[i+1] 
+            arreglo[i+1] = aux
+          
+
+
+print("arreglo modificado",arreglo)
